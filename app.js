5318dc234dee5f3ece2feca7b14a648227f0edce
martinkaldramov <martinkaldramov1@gmail.com>
2023-10-17 19:38:50 +0300

    Changed names

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-import express, { json } from 'express'
-import { createMovieRouter } from './routes/movies.js'
-import { corsMiddleware } from './middlewares/cors.js'
-import 'dotenv/config.js'
+import { createApp } from './app_1.js'
+import { MovieModel } from './models/local-file-system/movie.js'
 
-export const createApp = ({ movieModel }) => {
-  const app = express()
-  app.use(json())
-  app.use(corsMiddleware())
-  app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
-
-  app.use('/movies', createMovieRouter({ movieModel }))
-
-  const PORT = process.env.PORT ?? 1234
-
-  console.log("!!! Logging PORT number !!!", PORT);
-
-  app.listen(PORT, () => {
-    console.log(`server listening on port http://localhost:${PORT}`)
-  })
-}
+createApp({ movieModel: MovieModel })
