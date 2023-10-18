d230fa3bc14fb9ead5a00b16e53cb53310047b4a
martinkaldramov <martinkaldramov1@gmail.com>
2023-10-18 20:09:52 +0300

    Testing env variables during build time

---
+++ hello.js
@@ -0,0 +1,27 @@
+console.log("Running env_var_test!");
+console.log("Running env_var_test!");
+console.log("Running env_var_test!");
+console.log("Running env_var_test!");
+console.log("Running env_var_test!");
+console.log("Running env_var_test!");
+console.log("Running env_var_test!");
+
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
+console.log("Value of NEXT_PUBLIC_NAME:", process.env.NEXT_PUBLIC_NAME);
